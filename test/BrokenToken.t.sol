ea64a8f74a030d12061b270336b2ea322eb8b2d9
James Wenzel <jameswenzel@users.noreply.github.com>
2023-09-26 10:07:39 -0700

    change remapping

--- test/BrokenToken.t.sol
+++ test/BrokenToken.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.17;
 
 import {Test} from "forge-std/Test.sol";
-import {BrokenToken} from "src/./BrokenToken.sol";
+import {BrokenToken} from "../src/BrokenToken.sol";
 
 contract BrokenTokenTest is Test {
     BrokenToken token;
